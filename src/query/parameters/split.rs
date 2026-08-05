425bc28cfdc24d013f5307fd677a13f0af362d6b
rengenkon <konnov.rinat.mail.ru@gmail.com>
2026-08-05 20:30:57 +0400

    fix borrowing

--- src/query/parameters/split.rs
+++ src/query/parameters/split.rs
@@ -3,6 +3,6 @@ pub mod counting;
 pub mod number;
 
 pub trait SplitStrategy {
-    fn add_spliterator(self: &Self, parts: Vec<&str>) -> String;
-    fn compute_length(self: &Self, parts: Vec<&str>) -> usize;
+    fn add_spliterator(self: &Self, parts: &Vec<&str>) -> String;
+    fn compute_length(self: &Self, parts: &Vec<&str>) -> usize;
 }
