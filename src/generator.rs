6f28aa81faa0947de224e00c545692af413d8bfa
rengenkon <konnov.rinat.mail.ru@gmail.com>
2026-09-05 22:06:10 +0400

    tipical data

--- src/generator.rs
+++ src/generator.rs
@@ -54,7 +54,43 @@ impl Dictionary {
         &['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     }
 
-        
+    fn lowercase() -> &'static [char] {
+        &[
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        ]
+    }
+
+    fn uppercase() -> &'static [char] {
+        &[
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+        ]
+    }
+
+    fn special_punctuation() -> &'static [char] {
+        &['!', '?', '.', ',', ';', ':']
+    }
+
+    fn special_math() -> &'static [char] {
+        &['_', '-', '@', '=', '+', '*', '/']
+    }
+
+    fn special_brackets() -> &'static [char] {
+        &['(', ')', '[', ']', '{', '}']
+    }
+
+    fn special_quotes() -> &'static [char] {
+        &['"', '\'', '`', '&']
+    }
+
+    fn special_hash_percent() -> &'static [char] {
+        &['#', '$', '%', '^']
+    }
+
+    fn special_escape() -> &'static [char] {
+        &['\\', '|', '~']
+    }
 }
 
 struct Generator {}
