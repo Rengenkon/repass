a92230135c98288a1d979f5837fb68b64053d697
rengenkon <konnov.rinat.mail.ru@gmail.com>
2026-09-05 22:13:44 +0400

    change type

--- src/generator.rs
+++ src/generator.rs
@@ -50,46 +50,46 @@ impl Dictionary {
         todo!()
     }
 
-    fn numbers(self: Self) -> &'static [char] {
-        &['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    fn numbers() -> &'static [&'static str] {
+        &["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     }
 
-    fn lowercase() -> &'static [char] {
+    fn lowercase() -> &'static [&'static str] {
         &[
-            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+            "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
         ]
     }
 
-    fn uppercase() -> &'static [char] {
+    fn uppercase() -> &'static [&'static str] {
         &[
-            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+            "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
         ]
     }
 
-    fn special_punctuation() -> &'static [char] {
-        &['!', '?', '.', ',', ';', ':']
+    fn special_punctuation() -> &'static [&'static str] {
+        &["!", "?", ".", ",", ";", ":"]
     }
 
-    fn special_math() -> &'static [char] {
-        &['_', '-', '@', '=', '+', '*', '/']
+    fn special_math() -> &'static [&'static str] {
+        &["_", "-", "@", "=", "+", "*", "/"]
     }
 
-    fn special_brackets() -> &'static [char] {
-        &['(', ')', '[', ']', '{', '}']
+    fn special_brackets() -> &'static [&'static str] {
+        &["(", ")", "[", "]", "{", "}"]
     }
 
-    fn special_quotes() -> &'static [char] {
-        &['"', '\'', '`', '&']
+    fn special_quotes() -> &'static [&'static str] {
+        &["\"", "'", "`", "&"]
     }
 
-    fn special_hash_percent() -> &'static [char] {
-        &['#', '$', '%', '^']
+    fn special_hash_percent() -> &'static [&'static str] {
+        &["#", "$", "%", "^"]
     }
 
-    fn special_escape() -> &'static [char] {
-        &['\\', '|', '~']
+    fn special_escape() -> &'static [&'static str] {
+        &["\\", "|", "~"]
     }
 }
 
