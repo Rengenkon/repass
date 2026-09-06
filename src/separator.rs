d02078d969310b32a98b0aaa9b1496a899690992
rengenkon <konnov.rinat.mail.ru@gmail.com>
2026-09-06 21:50:09 +0400

    rewrite for dyn compatibility

--- src/separator.rs
+++ src/separator.rs
@@ -5,37 +5,31 @@ pub mod no_split;
 pub mod fix_count;
 pub mod on_parts;
 
-trait SeparatorInternal {
-    fn get_summary_length(parts: &[&str]) -> usize {
-        parts.iter().map(|part| part.len()).sum::<usize>()
+fn get_summary_length(parts: &[&str]) -> usize {
+    parts.iter().map(|part| part.len()).sum::<usize>()
+}
+
+fn format_error_msg(errors: &Vec<IllegalArgumentError>) -> String {
+    let mut result = String::from("Detected this errors for current Separator and parts:");
+    for error in errors {
+        result.push_str("\n\t-");
+        result.push_str(&error.to_string());
     }
+    result.push_str("\n\n");
+    result
+}
 
-    fn format_error_msg(errors: &Vec<IllegalArgumentError>) -> String {
-        let mut result = String::from("Detected this errors for current Separator and parts:");
-        for error in errors {
-            result.push_str("\n\t-");
-            result.push_str(&error.to_string());
-        }
-        result.push_str("\n\n");
-        result
-    }
-
-    fn add_separator(self: &Self, parts: &[&str]) -> String;
-    fn length_with_separators(self: &Self, parts: &[&str]) -> usize;
-
-    fn chack_errors(self: &Self, parts: &[&str]) -> Vec<IllegalArgumentError>;
-
-    fn panic_with_errors<T, F>(self: &Self, parts: &[&str], f: F) -> T
-    where
-        F: Fn(&Self, &[&str]) -> T,
-    {
-        let errors = self.chack_errors(parts);
-        if errors.is_empty() {
-            return f(self, parts)
-        }
-        let errors_msg = Self::format_error_msg(&errors);
+fn try_panic(errors: &Vec<IllegalArgumentError>) {
+    if !errors.is_empty() {
+        let errors_msg = format_error_msg(&errors);
         panic!("{}", errors_msg)
     }
+}
+
+pub trait SeparatorInternal {
+    fn add_separator(self: &Self, parts: &[&str]) -> String;
+    fn length_with_separators(self: &Self, parts: &[&str]) -> usize;
+    fn chack_errors(self: &Self, parts: &[&str]) -> Vec<IllegalArgumentError>;
 }
 
 /// Determine public functions for working with 'Separators'
@@ -44,12 +38,16 @@ trait SeparatorInternal {
 pub trait SeparatorStrategy: SeparatorInternal {
     /// Separate sequence with setuped separator segment
     fn separate(self: &Self, parts: &[&str]) -> String {
-        self.panic_with_errors(parts, Self::add_separator)
+        let errors = self.chack_errors(parts);
+        try_panic(&errors);
+        self.add_separator(parts)
     }
 
     /// Compute final length of sequence with separators
     fn length_after_separate(self: &Self, parts: &[&str]) -> usize {
-        self.panic_with_errors(parts, Self::length_with_separators)
+        let errors = self.chack_errors(parts);
+        try_panic(&errors);
+        self.length_with_separators(parts)
     }
 
     /// Check common errors in Separator or parts
