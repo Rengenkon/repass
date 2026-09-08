965b31e73093aa52df3fa6a235a3806aaa4c5a96
rengenkon <konnov.rinat.mail.ru@gmail.com>
2026-09-08 19:41:14 +0400

    write impl

--- src/separator/interval.rs
+++ src/separator/interval.rs
@@ -1,18 +1,27 @@
-use super::{IllegalArgumentError, SeparatorStrategy, SeparatorInternal};
+use super::{DEFAULT_SEPARATOR, IllegalArgumentError, Separator, SeparatorInternal};
 use core::str;
 use std::cmp::Ordering;
 
 #[derive(Debug)]
 pub struct FixIntervalSeparator<'a> {
     separator: &'a str,
-    length: usize,
+    interval: usize,
 }
 
 impl<'a> FixIntervalSeparator<'a> {
-    pub fn new(separator: &'a str, length: usize) -> Self {
+    pub fn new(separator: &'a str, interval: usize) -> Self {
         Self {
             separator,
-            length,
+            interval,
+        }
+    }
+}
+
+impl<'a> Default for FixIntervalSeparator<'a> {
+    fn default() -> Self {
+        Self {
+            separator: DEFAULT_SEPARATOR,
+            interval: 5,
         }
     }
 }
@@ -23,7 +32,7 @@ impl SeparatorInternal for FixIntervalSeparator<'_> {
         let mut iter = parts.iter();
         let mut cmp_result = Ordering::Less;
         let mut value = "";
-        let mut diff = self.length;
+        let mut diff = self.interval;
 
         loop {
             if cmp_result != Ordering::Greater {
@@ -34,11 +43,11 @@ impl SeparatorInternal for FixIntervalSeparator<'_> {
                 value = part.unwrap();
                 if !value.is_empty() && cmp_result == Ordering::Equal {
                     separated.push_str(self.separator);
-                    diff = self.length;
+                    diff = self.interval;
                 }
             } else {
                 separated.push_str(self.separator);
-                diff = self.length;
+                diff = self.interval;
             }
             cmp_result = value.len().cmp(&diff);
             match cmp_result {
@@ -54,17 +63,15 @@ impl SeparatorInternal for FixIntervalSeparator<'_> {
             }
         }
         separated
-
     }
 
     fn length_with_separators(self: &Self, parts: &[&str]) -> usize {
         let sum_length = super::get_summary_length(parts);
-        let mut separators_count = sum_length / self.length;
-        if separators_count > 0 && sum_length % self.length == 0 {
+        let mut separators_count = sum_length / self.interval;
+        if separators_count > 0 && sum_length % self.interval == 0 {
             separators_count -= 1;
         }
         sum_length + self.separator.len() * separators_count
-
     }
 
     fn chack_errors(self: &Self, parts: &[&str]) -> Vec<IllegalArgumentError> {
@@ -72,7 +79,7 @@ impl SeparatorInternal for FixIntervalSeparator<'_> {
         if self.separator.is_empty() {
             errors.push(IllegalArgumentError::EmptySeparator)
         }
-        if self.length == 0 {
+        if self.interval == 0 {
             errors.push(IllegalArgumentError::AdditionalParameterIsZero)
         }
         if parts.is_empty() {
@@ -85,50 +92,63 @@ impl SeparatorInternal for FixIntervalSeparator<'_> {
     }
 }
 
-impl SeparatorStrategy for FixIntervalSeparator<'_> {}
+impl Separator for FixIntervalSeparator<'_> {
+    fn try_compute_free_space(self: &Self, target_length: usize) -> Option<usize> {
+        if target_length == 0 {
+            return None;
+        }
+        let pairs_len = target_length - 1;
+        let one_pair_len = self.interval + self.separator.len();
+        let count = pairs_len / one_pair_len;
+        if target_length - count * one_pair_len > self.interval {
+            return None;
+        }
+        Some(target_length - count * self.separator.len())
+    }
+}
 
 #[cfg(test)]
 mod tests {
-    use rstest::{fixture, rstest};
     use super::*;
+    use rstest::{fixture, rstest};
 
     #[fixture]
     fn without_separator<'a>() -> FixIntervalSeparator<'a> {
-        FixIntervalSeparator{
+        FixIntervalSeparator {
             separator: "",
-            length: 1
+            interval: 1,
         }
     }
 
     #[fixture]
     fn without_count<'a>() -> FixIntervalSeparator<'a> {
-        FixIntervalSeparator{
+        FixIntervalSeparator {
             separator: "biba",
-            length: 0
+            interval: 0,
         }
     }
 
     #[fixture]
     fn one_dash<'a>() -> FixIntervalSeparator<'a> {
-        FixIntervalSeparator{
+        FixIntervalSeparator {
             separator: "-",
-            length: 1
+            interval: 1,
         }
     }
 
     #[fixture]
     fn one_long<'a>() -> FixIntervalSeparator<'a> {
-        FixIntervalSeparator{
+        FixIntervalSeparator {
             separator: "biba",
-            length: 1
+            interval: 1,
         }
     }
 
     #[fixture]
     fn multi_long<'a>() -> FixIntervalSeparator<'a> {
-        FixIntervalSeparator{
+        FixIntervalSeparator {
             separator: "biba",
-            length: 3
+            interval: 3,
         }
     }
 
@@ -141,10 +161,7 @@ mod tests {
     #[case(vec!["biba", "boba",])]
     #[should_panic]
     fn invalid_separator_panic_test(
-        #[values(
-            without_separator(),
-            without_count(),
-        )] separator: FixIntervalSeparator,
+        #[values(without_separator(), without_count())] separator: FixIntervalSeparator,
         #[case] input: Vec<&str>,
     ) {
         separator.separate(&input);
@@ -156,11 +173,7 @@ mod tests {
     #[case(vec!["", "",])]
     #[should_panic]
     fn invalid_data_panic_test(
-        #[values(
-            one_dash(),
-            one_long(),
-            multi_long(),
-        )] separator: FixIntervalSeparator,
+        #[values(one_dash(), one_long(), multi_long())] separator: FixIntervalSeparator,
         #[case] input: Vec<&str>,
     ) {
         separator.separate(&input);
@@ -174,11 +187,7 @@ mod tests {
     #[case(vec!["1", "2", "3", "4",])]
     #[case(vec!["biba", "boba",])]
     fn length_test(
-        #[values(
-            one_dash(),
-            one_long(),
-            multi_long(),
-        )] separator: FixIntervalSeparator,
+        #[values(one_dash(), one_long(), multi_long())] separator: FixIntervalSeparator,
         #[case] input: Vec<&str>,
     ) {
         let result_len = separator.separate(&input).len();
@@ -201,7 +210,7 @@ mod tests {
         let result = separator.separate(&input);
         assert_eq!(result, output);
     }
-    
+
     #[rstest]
     #[case(vec!["1",], "1")]
     #[case(vec!["biba",], "bbibaibibabbibaa")]
